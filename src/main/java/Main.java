dbade1e1050a22fd29f5bae34fd26447f2bea658
Tarutani <bp21028@shibaura-it.ac.jp>
2024-01-13 14:56:53 +0900

    環境構成

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+package src.main.java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // 数値の定義
+        int number1 = 10;
+        int number2 = 6;
+
+        // 加算
+        int sum = number1 + number2;
+        System.out.println("加算の結果: " + sum);
+
+        // 減算
+        int difference = number1 - number2;
+        System.out.println("減算の結果: " + difference);
+
+        // 乗算
+        int product = number1 * number2;
+        System.out.println("乗算の結果: " + product);
+
+        // 確認
+        System.out.println("pushできてるのか");
+    }
+}
